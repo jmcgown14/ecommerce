c7bdc2d933561a3d239462645938fc995b3a1237
Johnny McGown <jmac3292@gmail.com>
2023-03-30 22:25:43 -0600

    Updated tag routes and attempted to fix Product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -44,16 +44,16 @@ router.get('/:id', async(req, res) => {
   }
 });
 
+/* req.body should look like this...
+  {
+    product_name: "Basketball",
+    price: 200.00,
+    stock: 3,
+    tagIds: [1, 2, 3, 4]
+  }
+*/
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -64,12 +64,13 @@ router.post('/', (req, res) => {
             tag_id,
           };
         });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
+        return ProductTag.bulkCreate(productTagIdArr)
+        .then((productTagIds) => res.status(200).json(productTagIds))
+      } else {
       // if no product tags, just respond
       res.status(200).json(product);
+      }
     })
-    .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
